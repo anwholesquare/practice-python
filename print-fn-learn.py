fb89660194a3c3e8d410bbed3fd091d8e0eff188
anwholesquare <63560260+anwholesquare@users.noreply.github.com>
2022-01-05 11:30:42 -0800

    Print function details python

---
+++ print-fn-learn.py
@@ -0,0 +1,20 @@
+#Output
+
+#print with newline
+print("Hello Anan")
+
+#print with ending space
+print("Hello World", end = " ", flush = True)
+
+#print with ending an exclamatory sign and newline
+print("Hello World", end = "!\n", flush = True)
+
+#print with string format specifier
+name = "User"
+print("Hello {} You're watching my code right now".format (name))
+
+circle_radius = 5
+print("Area: {} sq unit".format(3.1416*circle_radius*circle_radius) )
+
+#print string with integer multiplication
+print("*" * 10)
